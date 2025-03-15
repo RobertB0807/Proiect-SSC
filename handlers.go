14d925efba68f926565a1df512c62b6f6d334480
RobertB0807 <robertalin2003@gmail.com>
2025-03-15 19:00:36 +0200

    Implemented the login function and the token uses is almost done

---
+++ handlers.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var jwtKey = []byte("secret_key")
+
+var users = map[string]string {
+	"user1": "password1",
+	"user2": "password2",
+}
+
+type Credentials struct {
+	Username string `json: "username"`
+	Password string `json: "password"`
+}
+
+type Claims struct {
+	Username string `json: "username"`
+	jwt.StandardClaims
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	var credentials Credentials
+	err := json.NewDecoder(r.Body).Decode(&credentials)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return 
+	}
+
+	//tot ce e de aici in jos sa ma mai uit peste pentru a putea intelege ce se intmpla in cod
+	expectedPassword, ok := users[credentials.Username]
+
+	if !ok || expectedPassword != credentials.Password {
+		w.WriteHeader(http.StatusUnauthorized)
+		return 
+	}
+
+	experationTime := time.Now().Add(time.Minute * 5)
+
+	claims := &Claims{
+		Username: credentials.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: experationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+func Refresh(w http.ResponseWriter, r *http.Request) {
+	
+}
